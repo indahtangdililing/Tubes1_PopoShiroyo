9dbf0555d375e8c3a097eb413c312cccdc3e523e
Kefas Kurnia Jonathan <19623006@std.stei.itb.ac.id>
2025-03-20 20:11:16 +0700

    refactor: renaming ZigiZagaBot

--- src/alternative-bots/ZigiZagaBot/ZigiZagaBot.cs
+++ src/alternative-bots/ZigiZagaBot/ZigiZagaBot.cs
@@ -4,11 +4,11 @@ using System.Drawing;
 
 public class ZigiZagaBot : Bot
 {
-    private double moveDistance = 1000;  // Jarak zigzag
-    private double turnAngle = 145;      // Sudut zigzag
+    private double moveDistance = 1000;  // Jarak Zigi-Zaga
+    private double turnAngle = 145;      // Sudut Zigi-Zaga
     private bool isMovingForward = true;
     private int moveCount = 0;
-    private readonly int MAX_MOVES = 3;  // Berapa kali zigzag sebelum mengubah arah
+    private readonly int MAX_MOVES = 3;  // Berapa kali Zigi-Zaga sebelum mengubah arah
 
     static void Main(string[] args)
     {
@@ -19,66 +19,52 @@ public class ZigiZagaBot : Bot
 
     public override void Run()
     {
-        // Set warna bot
         BodyColor = Color.Red;
         TurretColor = Color.Blue;
         RadarColor = Color.Red;
         ScanColor = Color.Blue;
 
-        // Set kecepatan putaran senjata
         GunTurnRate = 15;
         
         while (IsRunning)
         {
-            // Implementasi gerakan zigzag
-            if (moveCount >= MAX_MOVES)
-            {
+            /* Algoritma Greedy: Selalu bergerak, dengan arah yang tidak bisa ditebak */
+            if (moveCount >= MAX_MOVES){
                 isMovingForward = !isMovingForward;
                 moveCount = 0;
             }
 
             // Gerak maju/mundur
-            if (isMovingForward)
-            {
+            if (isMovingForward){
                 Forward(moveDistance);
             }
-            else
-            {
+            else {
                 Back(moveDistance);
             }
 
             // Berbelok ke kanan/kiri secara bergantian
-            if (moveCount % 2 == 0)
-            {
+            if (moveCount % 2 == 0){
                 TurnRight(turnAngle);
             }
-            else
-            {
+            else {
                 TurnLeft(turnAngle);
             }
-
-            // Putar radar terus menerus
-            //TurnRadarRight(360);
-            
             moveCount++;
         }
     }
 
-    public override void OnScannedBot(ScannedBotEvent evt)
-    {
+    /* Algoritma Greedy: Ketika melihat musuk langsung menembak */
+    public override void OnScannedBot(ScannedBotEvent evt){
         Fire(3);
     }
 
-    public override void OnHitWall(HitWallEvent e)
-    {
+    public override void OnHitWall(HitWallEvent e){
         TurnRight(30);
         Forward(50);
         moveCount = 0;
     }
 
-    public override void OnHitBot(HitBotEvent e)
-    {
-        // Hindari bot lain
+    public override void OnHitBot(HitBotEvent e){
         TurnRight(30);
         Forward(50);
     }
